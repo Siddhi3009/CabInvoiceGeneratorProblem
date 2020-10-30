f592c00a0023d67eb632d20bb2d6f8aab5d0be48
Administrator <siddhiseth3009@gmail.com>
2020-10-31 02:13:13 +0530

    [Siddhi]Add. EnhancedInvoiceSummary of all the rides calculation method added

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -41,5 +41,21 @@ namespace CabInvoiceGeneratorTest
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
             Assert.AreEqual(expected, summary.totalFare);
         }
+        [Test]
+        public void GivenUserIdWith5Rides_ShouldReturnEnhancedInvoiceSummary()
+        {
+            Ride[] rides =
+            {
+                new Ride(1.0, 1),
+                new Ride(2.0, 2),
+                new Ride(2.0, 2),
+                new Ride(4.0, 4),
+                new Ride(3.0, 3)
+            };
+            InvoiceSummary expected = new InvoiceSummary(5, 132);
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            Assert.AreEqual(summary, expected);
+        }
     }
 }
